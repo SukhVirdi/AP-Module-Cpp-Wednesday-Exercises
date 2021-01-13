5706491f4cfe1c73ea92c83a3329092db44c8f00
SukhVirdi <56550489+SukhVirdi@users.noreply.github.com>
2021-01-13 14:26:54 +0000

    Update exercise-1.cpp

--- exercise-1/exercise-1.cpp
+++ exercise-1/exercise-1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 struct stat buf;
 //stat function to obtain information
-bool isFileCheck(const string & name) {
+bool fileExists(const string & name) {
   return (stat(name.c_str(), & buf) == 0);
 }
 //takes file name
@@ -19,7 +19,7 @@ int main() {
   cout << "Filename to check: ";
   cin >> filename;
   //if statement to check the file name exists and prints to the user the required output 
-  if (isFileCheck(filename)) {
+  if (fileExists(filename)) {
     cout << "\n" << filename << ", " << buf.st_size << " bytes, last modified: " << ctime( & buf.st_mtime) << "\n\n";
   } else {
     cout << filename << " does not exist\n\n";
