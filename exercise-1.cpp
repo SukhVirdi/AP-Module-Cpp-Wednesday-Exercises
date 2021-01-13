f23682310ec28651cd97f688c958250a929bb9da
SukhVirdi <56550489+SukhVirdi@users.noreply.github.com>
2021-01-13 12:16:34 +0000

    Create exercise-1.cpp

---
+++ exercise-1.cpp
@@ -0,0 +1,29 @@
+//https://repl.it/join/olljggvx-sukhvirdi7
+//I researched stackoverflow and various websites to look at some examples on how to display the status of a file including the size and modified date.
+//I found that the stat function provided the outcome and utilised it to take the file name and further informatipon
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+struct stat buf;
+//stat function to obtain information
+bool isFileCheck(const string & name) {
+  return (stat(name.c_str(), & buf) == 0);
+}
+//takes file name
+int main() {
+  string filename;
+  //user input
+  cout << "Filename to check: ";
+  cin >> filename;
+  //if statement to check the file name exists and prints to the user the required output 
+  if (isFileCheck(filename)) {
+    cout << "\n" << filename << ", " << buf.st_size << " bytes, last modified: " << ctime( & buf.st_mtime) << "\n\n";
+  } else {
+    cout << filename << " does not exist\n\n";
+  }
+
+  return 0;
+}
